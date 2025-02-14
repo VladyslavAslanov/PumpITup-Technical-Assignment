39c25e25ff8deae081849adac0b98d6ef81d8e3c
vladyslav-aslanov <vlad.aslanov@icloud.com>
2025-02-14 20:43:38 +0100

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
 
 const Header = () => {
-  return <Typography variant="h5">Share Project</Typography>
+  return (
+    <Box>
+      <Typography variant="h5" component="h1" className="font-semibold text-gray-800">
+        Share Project
+      </Typography>
+    </Box>
+  )
 }
 
 export default Header
