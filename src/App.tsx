45a80baab9a1c5c22538869f4064fbce9fd6b152
vladyslav-aslanov <vlad.aslanov@icloud.com>
2025-02-14 16:53:42 +0100

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Footer from './components/Footer.tsx'
 
 const App = () => {
   return (
-    <Box className="flex flex-col gap-8 rounded-lg shadow-lg w-xl bg-white">
-      <Typography variant="h2">Share Project</Typography>
+    <Box className="flex flex-col gap-8 rounded-lg shadow-lg w-xl bg-white p-8">
+      <Typography variant="h5">Share Project</Typography>
       <PrivacySettings />
       <Footer />
     </Box>
